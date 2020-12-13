acba1b941d6c0fbe45afc0a2857be3f36e62ff39
da4818 <diadegoke@gmail.com>
2020-12-13 23:56:36 +0000

    sql mod- varchar

--- src/main/java/DataBase.java
+++ src/main/java/DataBase.java
@@ -21,7 +21,7 @@ public class DataBase {
 
         try {
             s = c.createStatement();
-            String sql = "CREATE TABLE USERS " +
+            String sql = "CREATE TABLE ACCOUNTS " +
                     "(ID INT PRIMARY KEY NOT NULL," +
                     " FNAME VARCHAR(50) NOT NULL, " +
                     " LNAME VARCHAR(50) NOT NULL, " +
@@ -29,14 +29,14 @@ public class DataBase {
                     " PASSW VARCHAR(50) NOT NULL)";
             s.executeUpdate(sql);
             s1 = c.createStatement();
-            String sql1 = "INSERT INTO USERS (ID,FNAME,LNAME,EMAIL,PASSW) VALUES (1,'John','Doe','johnappleseed@gmail.com','pass123');";
+            String sql1 = "INSERT INTO ACCOUNTS (ID,FNAME,LNAME,EMAIL,PASSW) VALUES (1,'John','Doe','johnappleseed@gmail.com','pass123');";
             s1.executeUpdate(sql1);
             s2 = c.createStatement();
-            ResultSet rs = s.executeQuery("SELECT * FROM USERS;");
+            ResultSet rs = s.executeQuery("SELECT * FROM ACCOUNTS;");
             while (rs.next()) {
                 int id = rs.getInt("id");
-                String fname = rs.getString("fname");
-                this.fname = fname;
+                String name = rs.getString("fname");
+                this.fname = name;
                 String lname = rs.getString("lname");
                 String email = rs.getString("email");
                 String pass = rs.getString("passw");
