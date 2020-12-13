5d4c148f6b480d4903197c85f0294860ab956215
da4818 <diadegoke@gmail.com>
2020-12-13 18:36:44 +0000

    Initial commit

---
+++ src/main/java/ServletHome.java
@@ -0,0 +1,21 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(urlPatterns ={"/home"},loadOnStartup = 1)
+public class ServletHome extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp);
+        resp.setContentType("text/html");
+        resp.getWriter().write("<html><title>Test></title><head><body><h1>Hello</h1></body></head></html>");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+}
