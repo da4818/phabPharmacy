f2df3b41548f0a28f29a857456ad55b6878923d6
da4818 <diadegoke@gmail.com>
2020-12-14 18:46:41 +0000

    loginform

--- src/main/java/SerlvetLogin.java
+++ src/main/java/SerlvetLogin.java
@@ -5,7 +5,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(name = "login")
+@WebServlet(urlPatterns ={"/login"},loadOnStartup = 0)
 public class SerlvetLogin extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
