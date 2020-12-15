53f39544e5c96ed254f2dbc7026e1389e5262f7f
da4818 <diadegoke@gmail.com>
2020-12-15 23:43:19 +0000

    home,login,register,browse - hover messages, all static header when 'active'

--- src/main/java/ServletRegister.java
+++ src/main/java/ServletRegister.java
@@ -95,7 +95,7 @@ public class ServletRegister extends HttpServlet {
                 "</head>\n" +
                 "<body>\n" +
                 "<div class=\"navbar\">\n" +
-                "    <a href=\"https://cmdmcpharmacywebsite.herokuapp.com/home\"><i class=\"fa fa-fw fa-home\"></i>Home</a>\n" +
+                "    <a href=\"https://phabpharmacy.herokuapp.com/home\"><i class=\"fa fa-fw fa-home\"></i>Home</a>\n" +
                 "    <div class=\"dropdown\">\n" +
                 "        <button style=\"cursor: pointer;\" class= \"dropbtn\" onclick=\"redirectBrowse()\"><i class=\"fa fa-fw fa-search\"></i>Browse<i class=\"fa fa-caret-down\"></i></button>\n" +
                 "        <div class=\"dropdown-content\">\n" +
