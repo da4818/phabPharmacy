12269d864d3e8f10638e72912f73bac0fcaaaebf
da4818 <diadegoke@gmail.com>
2020-12-22 01:10:50 +0000

    home,login,register,browse - hover messages, all static header when 'active', browse basket (div1), min quantity=0 LoginDAO, create database -login & register automatic browse data, output string, post request in same page

--- src/main/java/Website/ServletRegister.java
+++ src/main/java/Website/ServletRegister.java
@@ -6,6 +6,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 @WebServlet(urlPatterns = "/register", loadOnStartup = 0)
 public class ServletRegister extends HttpServlet {
@@ -15,6 +16,36 @@ public class ServletRegister extends HttpServlet {
         String output = htmlOutput();
         resp.getWriter().write(output);
     }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        String HTML = htmlOutput();
+        PrintWriter writer = resp.getWriter();
+        String fn = req.getParameter("fname");
+        String ln = req.getParameter("lname");
+        String em = req.getParameter("email");
+        String pw = req.getParameter("pass");
+        String vpw = req.getParameter("verifyPass");
+        String cn = req.getParameter("cardno");
+        String ad = req.getParameter("postcode");
+        writer.println(HTML);
+        if(LoginDAO.validate(em,pw)){ //create validation to see if email exists
+            writer.println("<h2> There is an existing account with the email entered. Please log in.</h2>");
+        }
+        else if (fn.isEmpty() || ln.isEmpty() || em.isEmpty() || pw.isEmpty() || vpw.isEmpty() || cn.isEmpty() || ad.isEmpty()){
+            writer.print("<h2>Incomplete fields, please enter all the information.</h2>");
+        }
+        else if (!pw.equals(vpw)){
+            writer.println("<h2> Passwords don't match. Please try again.</h2>");
+        }
+        else{
+            LoginDAO.addUser(fn,ln,em,pw,cn,ad);
+            String output= LoginDAO.getName(em,pw);
+            writer.print("<h2>Succesful registration. Welcome, " + output + "</h2>");
+        }
+    }
+
     public String htmlOutput(){
         String output = "<!DOCTYPE html>\n" +
                 "<html>\n" +
