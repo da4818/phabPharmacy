ac602a5151b4e213063a80fbf8fcb1f700da122c
da4818 <diadegoke@gmail.com>
2021-01-03 22:12:06 +0000

    Home, Login, Register, Browse, Basket, Order servlets LoginDAO: login/register validation check, create tables with initial entries, add values to user/basket database, reset table content Basket icon number Current user shown Update user basket Order confirmation display user info Email and EmailValidation classes - used for /register Log out feature

--- src/main/java/Website/Servlets/ServletBasket.java
+++ src/main/java/Website/Servlets/ServletBasket.java
@@ -69,12 +69,15 @@ public class ServletBasket extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.setContentType("text/html");
-        String t = req.getParameter("update");
-        //int basketItemId = Integer.parseInt(req.getParameter("basketItemId"));
+        String logOut = req.getParameter("logOut");
+        if (logOut.equals("Log Out")){
+           LoginDAO.resetTable("logged");
+        }
+        String updateBasket = req.getParameter("update");
         int q = Integer.parseInt(req.getParameter("basketItemQuantity"));
         int basketButtonNumber = Integer.parseInt(req.getParameter("basketButtonNumber"));
         Product modifiedItem = LoginDAO.getBasketInfo(basketButtonNumber);
-        if (t.equals("Update")) {
+        if (updateBasket.equals("Update")) {
             LoginDAO.addToBasket(modifiedItem,q);
         }
         else {
@@ -130,11 +133,20 @@ public class ServletBasket extends HttpServlet {
 
     public String htmlOutput(){
         boolean userLoggedIn = LoginDAO.checkLoggedIn();
-        String userMessage = "";
+        String displayCurrentUser = "";
         User cUser = null;
         if (userLoggedIn == true) {
             cUser = LoginDAO.getCurrentUser();
-            userMessage = cUser.fname;
+            displayCurrentUser = "     <form name=\"logOut\" action=\"home\" method=\"post\">\n" +
+                    "       <div style=\"float: right;\" class=\"currentUser\">" + cUser.fname + "<i class=\"fa fa-fw fa-user\"></i>\n" +
+                    "           <div class=\"logOut\">\n" +
+                    "               <input class=\"logOutButton\" type=\"submit\" value=\"Log Out\">\n" +
+                    "           </div>\n" +
+                    "       </div>\n" +
+                    "    </form>\n";
+        }
+        else if (userLoggedIn == false){
+            displayCurrentUser = "<div class=\"currentUser\"><i class=\"fa fa-fw fa-user\"></i></div>";
         }
         int basketSize = LoginDAO.getBasketSize();
         String basketSizeOut="";
@@ -248,6 +260,33 @@ public class ServletBasket extends HttpServlet {
                 "            width: 80px;\n" +
                 "            height: 45px;\n" +
                 "        }\n" +
+                "        .currentUser{\n" +
+                "            position: relative;\n" +
+                "            float: right;\n" +
+                "            font-size: 16px;\n" +
+                "            color: white;\n" +
+                "            text-align: center;\n" +
+                "            padding: 14px 16px 4px 16px;\n" +
+                "            text-decoration: none;\n" +
+                "        }\n" +
+                "        .logOut{\n" +
+                "            position: absolute:\n" +
+                "            height: 10px;\n" +
+                "            bottom: 0px;\n" +
+                "            margin: 0px;\n" +
+                "            border: none;\n" +
+                "            background-color: transparent;\n" +
+                "            border: none;\n" +
+                "            font-size: 8px;\n" +
+                "            color: white;\n" +
+                "        }\n" +
+                "        .logOutButton{\n" +
+                "            background-color: transparent;\n" +
+                "            font-size: 8px;\n" +
+                "            color: white;\n" +
+                "            margin: 0px;\n" +
+                "            border: none;\n" +
+                "        }\n" +
                 "        .buttonStyle{\n" +
                 "            background-color: #51B5C2; \n" +
                 "            border: none;\n" +
@@ -308,7 +347,7 @@ public class ServletBasket extends HttpServlet {
                 "    <a href=\"https://phabpharmacy.herokuapp.com/login\"><i class=\"fa fa-fw fa-user\"></i>Login</a>\n" +
                 "    <a href=\"https://phabpharmacy.herokuapp.com/register\"><i class=\"fa fa-fw fa-user-plus\"></i>Register</a>\n" +
                 "    <a href=\"https://phabpharmacy.herokuapp.com/basket\" style=\"background-color: #00B8C5; width: 35px;\"><i style=\"width: 35px;\" class=\"fa fa-fw fa-shopping-basket\"><p style=\"display: inline; font-family: Arial; font-weight: bold\" id=\"basket\"> " + basketSizeOut + "</p></i></a>\n" +
-                "    <div class=\"currentUser\">" + userMessage + "<i class=\"fa fa-fw fa-user\"></i></div>\n" +
+                displayCurrentUser +
                 "</div>\n" +
                 "<h1>Shopping Basket</h1>\n";
     }
