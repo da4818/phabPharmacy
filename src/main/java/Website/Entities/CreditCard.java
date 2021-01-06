c90727c6cfe7ef7573585662936992caa7e51823
da4818 <diadegoke@gmail.com>
2021-01-06 21:32:39 +0000

    Home, Login, Register, Browse, Basket, Order servlets LoginDAO: login/register validation check, create tables with initial entries, add values to user/basket database, reset table content Basket icon number Current user shown Update user basket Order confirmation display user info Email and EmailValidation classes - used for Register servlet Log out feature Edit User details Unit testing Map -safe

--- src/main/java/Website/Entities/CreditCard.java
+++ src/main/java/Website/Entities/CreditCard.java
@@ -7,15 +7,69 @@ public class CreditCard {
     public String accountNumber;
 
     public CreditCard(String cardNumber, String cvv, String sortCode, String accountNumber){
-        this.cardNumber = cardNumber;
+        this.cardNumber = cardNumber.replaceAll("\\s",""); //Removes any spaces
         this.cvv = cvv;
-        this.sortCode = sortCode;
+        //If the user inputs their sort code as xx-xx-xx, it will be trimmed to remove all '-'s
+        if(sortCode.indexOf('-') > 0){
+            this.sortCode = sortCode.replace("-","");
+        }
+        else {
+            this.sortCode = sortCode;
+        }
         this.accountNumber = accountNumber;
     }
-
+    //Checks the cardnumber is the correct length, and is only number
     public boolean validCardNumber(){
         if (cardNumber.length() == 16){
             if (!isNumeric(cardNumber)){
+                return false;
+            }
+            else{
+                return true;
+            }
+        }
+        else{
+            return false;
+        }
+    }
+    public boolean validCvv(){
+        if (cvv.length() == 3){
+            if (!isNumeric(cvv)){
+                return false;
+            }
+            else{
+                return true;
+            }
+        }
+        else{
+            return false;
+        }
+    }
+
+    public String getSortCode() {
+        return sortCode;
+    }
+
+    public String getCardNumber() {
+        return cardNumber;
+    }
+
+    public boolean validAccountNumber(){
+        if (accountNumber.length() == 8){
+            if (!isNumeric(accountNumber)){
+                return false;
+            }
+            else{
+                return true;
+            }
+        }
+        else{
+            return false;
+        }
+    }
+    public boolean validSortCode(){
+        if (sortCode.length() == 6){
+            if (!isNumeric(sortCode)){
                 return false;
             }
             else{
@@ -31,6 +85,7 @@ public class CreditCard {
         out +="xxxx";
         return out;
     }
+
 
     public boolean isNumeric(String input){
         for (char c:input.toCharArray()){
--- src/test/java/TestCreditCard.java
+++ src/test/java/TestCreditCard.java
@@ -7,5 +7,9 @@ public class TestCreditCard {
     public void testCreditCard(){
         CreditCard ci = new CreditCard("1234567812345678","123","12-34-56","12345678");
         Assert.assertEquals(ci.getCensoredCardNumber(),"123456781234xxxx");
+        Assert.assertEquals(ci.getSortCode(),"123456");
+
+        CreditCard ci1 = new CreditCard("1234 5678 1234 5678","123","12-34-56","12345678");
+        Assert.assertEquals(ci1.getCardNumber(),"1234567812345678");
     }
 }
