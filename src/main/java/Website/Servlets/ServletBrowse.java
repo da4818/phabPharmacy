2fb944087971373c55d066144fa063f720c6d8eb
da4818 <diadegoke@gmail.com>
2021-01-08 20:01:08 +0000

    -  integrated functions: MailSender has import errors Home, Login, Register, Browse, Basket, Order servlets LoginDAO: login/register validation check, create tables with initial entries, add values to user/basket database, reset table content Basket icon number Current user shown Update user basket Order confirmation display user info Email and EmailValidation classes - used for Register servlet Log out feature Edit User details Unit testing Map

--- src/main/java/Website/Servlets/ServletBrowse.java
+++ src/main/java/Website/Servlets/ServletBrowse.java
@@ -24,7 +24,7 @@ public class ServletBrowse extends HttpServlet {
         ArrayList<String> headers = getHeaderinfo("headers"); //See line 110
         ArrayList<String> headerURLs = getHeaderinfo("headerURLs"); //See line 110
         int j=1;
-        for (int i=0;i<42;i++){
+        for (int i=0;i<24;i++){
             Product p = LoginDAO.getProduct(i);
             resp.getWriter().write(p.brand + " " + p.name);
 
