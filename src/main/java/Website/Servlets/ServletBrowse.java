ad3de4071e110482ec601a2915226cabc324be23
da4818 <diadegoke@gmail.com>
2021-01-09 12:18:49 +0000

    -  integrated functions: MailSender has import errors Home, Login, Register, Browse, Basket, Order servlets LoginDAO: login/register validation check, create tables with initial entries, add values to user/basket database, reset table content Basket icon number Current user shown Update user basket Order confirmation display user info Email and EmailValidation classes - used for Register servlet Log out feature Edit User details Unit testing Map

--- src/main/java/Website/Servlets/ServletBrowse.java
+++ src/main/java/Website/Servlets/ServletBrowse.java
@@ -22,9 +22,12 @@ public class ServletBrowse extends HttpServlet {
         resp.getWriter().write(HTML);
         ArrayList<String> headers = getHeaderinfo("headers"); //See line 110
         ArrayList<String> headerURLs = getHeaderinfo("headerURLs"); //See line 110
-        int j=1;
+        //int j=1;
+        for(int j =1;j<42;j++){
         Product p = LoginDAO.getProduct(j);
-        while (j<42) { //Number of items in paddington store *41*
+        resp.getWriter().write("<p>" +p.brand+ "-"+p.name+"</p>");
+        }
+        /*while (j<42) { //Number of items in paddington store *41*
             for (int i = 0; i < 6; i++) {
                 resp.getWriter().write("<section>\n" +
                         "<h2 id=\"" + headerURLs.get(i) + "\">" + headers.get(i) + "</h2>\n"); //See line 110
@@ -55,7 +58,7 @@ public class ServletBrowse extends HttpServlet {
                 }
                 resp.getWriter().write("</section>");
             }
-        }
+        }*/
         resp.getWriter().write("</body>\n" + "</html>");
     }
 
