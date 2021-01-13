7459f4fb217d84641ce606fcea568b04f2f0bb64
da4818 <diadegoke@gmail.com>
2021-01-13 13:38:10 +0000

    -  integrated functions: MailSender has import errors Home, Login, Register, Browse, Basket, Order servlets LoginDAO: login/register validation check, create tables with initial entries, add values to user/basket database, reset table content Basket icon number Current user shown Update user basket Order confirmation display user info Email and EmailValidation classes - used for Register servlet Log out feature Edit User details Unit testing Map

--- src/main/java/Website/Servlets/ServletOrder.java
+++ src/main/java/Website/Servlets/ServletOrder.java
@@ -30,10 +30,13 @@ public class ServletOrder extends HttpServlet {
         String total = df.format(totalBasket);
         User u = LoginDAO.getCurrentUser();
         Address ad = new Address(u.address,u.postcode);
-
+        String phoneNumber="";
+        if (!u.phoneno.isEmpty()){
+            phoneNumber = u.phoneno;
+        }
         resp.getWriter().write("<div class=\"addressContainer\">\n" +
                 "  <p style=\"display: inline-block; margin-bottom: 0px;\"><b>Shipping Address</b></p>\n" +
-                "  <p>" + u.fname + " " + u.lname + "<br>" + u.postcode +"<br>" + u.phoneno + "</p>\n" +
+                "  <p>" + u.fname + " " + u.lname + "<br>" + ad.getAddress() + u.postcode +"<br>" + phoneNumber + "</p>\n" +
                 "  <p><b>Payment Details</b></p>\n" +
                 "  <p>" + cc.getCensoredCardNumber() + "<br>" + cc.getSortCode() + "<br>" + cc.accountNumber +"</p>\n" +
                 "  <button onclick=\"window.location.href='https://phabpharmacy.herokuapp.com/amend_details';\"class=\"buttonStyle\">Edit Details</button>\n" +
