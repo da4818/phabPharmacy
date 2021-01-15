05dc931cce94214447eb3d922d3d5292ece399ab
da4818 <diadegoke@gmail.com>
2021-01-15 00:27:16 +0000

    Home, Login, Register, Browse, Basket, Order servlets LoginDAO: login/register validation check, create tables with initial entries, add values to user/basket database, reset table content Basket icon number Current user shown Update user basket Order confirmation display user info Email and EmailValidation classes - used for Register servlet Log out feature Edit User details Unit testing Map

--- src/main/java/Website/Functions/UpdateQuantity.java
+++ src/main/java/Website/Functions/UpdateQuantity.java
@@ -18,7 +18,7 @@ public class UpdateQuantity {
             Class.forName("org.postgresql.Driver");
             Connection c = DriverManager.getConnection(dbUrl);
             Statement stmt = c.createStatement();
-            //Get the details from the database about the specific product
+            // Get the details from the database about the specific product
             String sqlStr = "SELECT * FROM shop_product WHERE name = " + name1 + " AND brand = " + brand1;
             System.out.println(sqlStr);
             ResultSet rs = stmt.executeQuery(sqlStr);
@@ -29,11 +29,11 @@ public class UpdateQuantity {
                 fullStock = rs.getInt("full_stock");
                 System.out.println(quant);
             }
-            //check if the product has been sold (e.g negative change in stock)
+            // Checks if the product has been sold (e.g negative change in stock)
             if (quantityChange < 0) {
                 float oldProfit = 0;
                 float newProfit = 0;
-                //get profit from day of sale
+                // Gets profit from day of sale
                 rs = stmt.executeQuery("SELECT * FROM daily_profit WHERE date = CURRENT_DATE ;");
                 if (rs.next()) {
                     oldProfit = rs.getFloat("profit");
@@ -42,19 +42,19 @@ public class UpdateQuantity {
                     oldProfit = 0;
                     stmt.execute("INSERT INTO daily_profit(date, profit) VALUES(CURRENT_DATE,0);");
                 }
-                //calculate change in profit based on the price of the drug and how much it sold for
+                // Calculate change in profit based on the price of the drug and how much it sold for
                 float saleProfit = -quantityChange * (sellPrice - buyPrice);
                 newProfit = oldProfit + saleProfit;
                 stmt.execute("UPDATE daily_profit SET profit = " + newProfit + " WHERE date = CURRENT_DATE;");
             }
             int update = quant + quantityChange;
-            //update the quantity of the product
+            // Update the quantity of the product
             stmt.execute("UPDATE shop_product SET quantity = " + update + " WHERE name = " + name1 + " AND brand = " + brand1 + ";");
             int hardMin = (int) (fullStock*0.1);
             if(update<hardMin){
                 List<Product> order = new ArrayList<>();
                 order.add(new Product(name1, brand1, fullStock-quant));
-                //we want to check all other members below their softmins e.g fullstock*0.2 and make order
+                // If other members are below their soft minimum (e.g fullstock*0.2), we'll make an order for them as well
                 ResultSet rset=stmt.executeQuery("SELECT * FROM shop_product WHERE id>0 ");
                 while(rset.next()){
                     int fullStock1 = (rset.getInt("full_stock"));
